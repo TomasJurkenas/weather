1026d8ed7e677cb3abdac85ffe33fc96be87799a
TomasJurkenas <tomisarius18@gmail.com>
2022-10-06 10:32:35 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+
+
+// import axios from "axios";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  componentDidMount() {
+
+    if ("geolocation" in navigator) {
+
+      console.log("Available");
+
+    } else {
+
+      console.log("Not Available");
+
+    }
+
+  }
+
+  // geras kvietimas viskas veikia !
+  // const options = {
+  //   method: 'GET',
+  //   url: 'https://api.ambeedata.com/weather/latest/by-lat-lng',
+  //   params: {lat: ' 54.687157', lng: '25.279652'},
+  //   headers: {'x-api-key': '3fe96bf3e5bd44af35b33481b154709fdea07726ed31f735b67045567ab46e80', 'Content-type': 'application/json'}
+  // };
+  // axios.request(options).then(function (response) {
+  //   console.log(response.data);
+  // }).catch(function (error) {
+  //   console.error(error);
+  // });
 }
 
 export default App;
